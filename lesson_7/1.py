b81f0e3b7b344173f37f421397de6ede8e4274a6
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2022-10-17 08:35:34 +0600

    lesson 7

---
+++ lesson_7/1.py
@@ -0,0 +1,13 @@
+a = [
+    [1, 2, 3, 4],
+    [0, -1, 99, 1],
+    [-1, 1, 1, 2],
+    [1, -1, 1000, 3]
+]
+
+s = 0
+
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        print(a[i][j], end=" ")
+    
