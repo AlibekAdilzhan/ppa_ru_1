3d399d3853d14ce434c845a70f05d6c21bc09e3e
AlibekAdilzhan <alibek.adilzhanov@gmail.com>
2022-11-21 08:46:22 +0600

    lesson 12_2

--- lesson_11/2.py
+++ lesson_11/2.py
@@ -7,9 +7,25 @@ class Hero:
         self.v_speed = v_speed
         self.h_speed = h_speed
         self.pic = pic
+        # self.can_jump = False
 
     def update(self):
-        self.x = self.x + self.h_speed
+        if keystate[pygame.K_RIGHT] == True:
+            self.x = self.x + self.h_speed
+        elif keystate[pygame.K_LEFT] == True:
+            self.x = self.x - self.h_speed
+        self.v_speed = self.v_speed + g
+        self.y = self.y + self.v_speed
+        if self.y >= ground:
+            self.v_speed = 0
+            self.y = ground    
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.v_speed = -20
+            if event.type == pygame.QUIT:
+                run = False
 
 
 pygame.init()
@@ -17,9 +33,12 @@ pygame.init()
 fps = 60
 clock = pygame.time.Clock()
  
+# game settings
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 block_size = 64
+g = 2
+ground = 400
 
 snowman = pygame.image.load("SnowMan1_right.png")
 snowman = pygame.transform.scale(snowman, (block_size, block_size))
@@ -35,8 +54,8 @@ run = True
 while run:
     screen.fill((100, 255, 255))
     
+    keystate = pygame.key.get_pressed()
     hero.update()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
